1dd22f8610a3ff28ca20757516914018aea1a78b
GreenTheColour1 <contact@camerongreen.ca>
2025-05-15 18:29:46 -0400

    add posts.go

---
+++ posts/posts.go
@@ -0,0 +1,18 @@
+package posts
+
+import (
+	"embed"
+	"time"
+)
+
+type Post struct {
+	Id         [32]byte  `field:"id"`
+	Created_at time.Time `field:"created_at"`
+	Updated_at time.Time `field:"updated_at"`
+	Title      string    `field:"title"`
+	Filename   string    `field:"filename"`
+	Slug       string    `field:"slug"`
+}
+
+// go:embed *.md
+var Assets embed.FS
