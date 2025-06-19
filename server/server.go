100045b838a5a20e3ac7c36302c627511a7e37bb
GreenTheColour1 <contact@camerongreen.ca>
2025-06-19 16:05:14 -0400

    testing css

--- server/server.go
+++ server/server.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/GreenTheColour1/go-blog/assets"
 	"github.com/GreenTheColour1/go-blog/database"
-	"github.com/GreenTheColour1/go-blog/posts"
 	"github.com/GreenTheColour1/go-blog/views"
 	"github.com/a-h/templ"
 )
@@ -14,22 +14,22 @@ type Server struct {
 }
 
 func (s *Server) Start() {
-	post := posts.Post{Title: "WOWEEEEE"}
-	http.Handle("/", templ.Handler(views.Home(post)))
+	mux := http.NewServeMux()
 
 	db := database.Connect()
 	defer db.DB.Close()
 
-	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/", templ.Handler(views.Home()))
+
+	mux.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
 		data, err := db.GetAllPosts()
-		log.Println(data)
 		if err != nil {
 			log.Fatal(err)
 		}
 		views.PostsBody(data).Render(r.Context(), w)
 	})
 
-	http.HandleFunc("/post/{slug}", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/post/{slug}", func(w http.ResponseWriter, r *http.Request) {
 		data, err := db.GetPostBySlug(r.PathValue("slug"))
 		if err != nil {
 			log.Fatal(err)
@@ -37,5 +37,7 @@ func (s *Server) Start() {
 		views.PostBody(*data).Render(r.Context(), w)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.FS(assets.Assets))))
+
+	http.ListenAndServe(":8080", mux)
 }
