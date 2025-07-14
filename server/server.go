793e5f085c9c63f266d3d5aef2992a2e7a1163f5
GreenTheColour1 <dev@camerongreen.ca>
2025-07-14 14:52:18 -0400

    make sure assets are not cached when developing

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/GreenTheColour1/go-blog/assets"
 	"github.com/GreenTheColour1/go-blog/database"
@@ -39,7 +40,18 @@ func (s *Server) Start() {
 		views.PostBody(*post).Render(r.Context(), w)
 	})
 
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.FS(assets.Assets))))
+	mux.Handle("/assets/", disableCacheInDevMode(http.StripPrefix("/assets/", http.FileServer(http.FS(assets.Assets)))))
 
 	http.ListenAndServe(":8080", mux)
+}
+
+func disableCacheInDevMode(next http.Handler) http.Handler {
+	devEnv, _ := os.LookupEnv("ENVIRONMENT")
+	if devEnv != "dev" {
+		return next
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
 }
