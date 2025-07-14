3c776cffa10ba1cd7425056ad51cf329389ecf82
GreenTheColour1 <dev@camerongreen.ca>
2025-07-13 20:24:09 -0400

    add markdown to HTML conversion

--- posts/posts.go
+++ posts/posts.go
@@ -1,8 +1,13 @@
 package posts
 
 import (
+	"bytes"
 	"embed"
 	"time"
+
+	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
+	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
 )
 
 type Post struct {
@@ -12,8 +17,28 @@ type Post struct {
 	Title      string    `field:"title"`
 	Filename   string    `field:"filename"`
 	Slug       string    `field:"slug"`
-	Body       string
+	Body       []byte
+	RawHTML    string
 }
 
 //go:embed files/*.md
 var PostAssets embed.FS
+
+func (p *Post) ConvertBodyToHTML() {
+	markdown := goldmark.New(goldmark.WithExtensions(
+		highlighting.NewHighlighting(
+			highlighting.WithStyle("gruvbox"),
+			highlighting.WithFormatOptions(
+				chromahtml.WithLineNumbers(true),
+			),
+		),
+	))
+
+	var buf bytes.Buffer
+
+	if err := markdown.Convert(p.Body, &buf); err != nil {
+		panic(err)
+	}
+
+	p.RawHTML = buf.String()
+}
